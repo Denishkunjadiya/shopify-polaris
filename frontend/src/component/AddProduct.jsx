fbee56c9f8a4b509452d08f0a43648adee770000
Denishkunjadiya <denishkunjadiya02@gmail.com>
2024-12-17 23:01:27 +0530

    `feat: updated AddProduct component with new form fields and CKEditor integration`

--- frontend/src/component/AddProduct.jsx
+++ frontend/src/component/AddProduct.jsx
@@ -6,21 +6,25 @@ import {
   ContextWatchdog,
   Essentials,
   Italic,
+  List,
   Paragraph,
 } from "ckeditor5";
-import React from "react";
 
 import {
+  Box,
   Card,
   FormLayout,
-  Grid,
+  InlineError,
+  Label,
   Layout,
   Page,
   TextField,
 } from "@shopify/polaris";
 import "ckeditor5-premium-features/ckeditor5-premium-features.css";
 import "ckeditor5/ckeditor5.css";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import ImageUpload from "./ImageUpload";
 
 const AddProduct = () => {
   const navigate = useNavigate();
@@ -33,15 +37,82 @@ const AddProduct = () => {
       <Layout>
         <Layout.Section>
           <Card sectioned>
-            <FormLayout>
-              <TextField
-                label="Title"
-                // onChange={() => {}}
-                autoComplete="off"
-              />
-            </FormLayout>
+            <Box style={{ marginBottom: "15px" }}>
+              <FormLayout>
+                <TextField
+                  label="Title"
+                  placeholder="Enter title"
+                  // onChange={() => {}}
+                  autoComplete="off"
+                  error="Store name is required"
+                />
+              </FormLayout>
+            </Box>
+            <Box style={{ marginBottom: "15px" }}>
+              <Label>Description</Label>
+              <Box
+                style={{
+                  border: "1px solid #999",
+                  marginTop: "5px",
+                  borderRadius: "10px",
+                  overflow: "hidden",
+                }}
+              >
+                <CKEditorContext
+                  context={Context}
+                  contextWatchdog={ContextWatchdog}
+                  onChangeInitializedEditors={(editors) => {
+                    console.info(
+                      editors.editor1?.instance,
+                      editors.editor1?.yourAdditionalData
+                    );
+                  }}
+                >
+                  <CKEditor
+                    editor={ClassicEditor}
+                    config={{
+                      plugins: [Essentials, Bold, Italic, Paragraph, List],
+                      toolbar: [
+                        "undo",
+                        "redo",
+                        "|",
+                        "bold",
+                        "italic",
+                        "bulletedList",
+                        "numberedList",
+                      ],
+                    }}
+                    data="<p>Hello from the first editor working with the context!</p>"
+                    contextItemMetadata={{
+                      name: "editor1",
+                      yourAdditionalData: 2,
+                    }}
+                    onReady={(editor) => {
+                      console.log("Editor 1 is ready to use!", editor);
+                    }}
+                  />
+                </CKEditorContext>
+              </Box>
+            </Box>
+            <Box style={{ marginBottom: "15px" }}>
+              <FormLayout>
+                <ImageUpload />
+              </FormLayout>
+            </Box>
+            <Box style={{ marginBottom: "15px" }}>
+              <FormLayout>
+                <TextField
+                  label="Category"
+                  placeholder="Enter title"
+                  // onChange={() => {}}
+                  autoComplete="off"
+                  error="Store name is required"
+                />
+              </FormLayout>
+            </Box>
           </Card>
         </Layout.Section>
+
         <Layout.Section variant="oneThird">
           <Card title="Tags" sectioned>
             <p>Add tags to your order.</p>
---
+++ frontend/src/component/ImageUpload.jsx
@@ -0,0 +1,47 @@
+import { BlockStack, DropZone, Text, Thumbnail } from "@shopify/polaris";
+import { NoteIcon } from "@shopify/polaris-icons";
+import { useCallback, useState } from "react";
+
+export default function ImageUpload() {
+  const [file, setFile] = useState();
+
+  const handleDropZoneDrop = useCallback(
+    (_dropFiles, acceptedFiles, _rejectedFiles) => setFile(acceptedFiles[0]),
+    []
+  );
+
+  const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
+
+  const fileUpload = !file && <DropZone.FileUpload />;
+  const uploadedFile = file && (
+    <BlockStack>
+      <Thumbnail
+        size="small"
+        alt={file.name}
+        source={
+          validImageTypes.includes(file.type)
+            ? window.URL.createObjectURL(file)
+            : NoteIcon
+        }
+      />
+      <div>
+        {file.name}{" "}
+        {/* <Text variant="bodySm" as="p">
+          {file.size} bytes
+        </Text> */}
+      </div>
+    </BlockStack>
+  );
+
+  return (
+    <DropZone
+      label="Media"
+      type="image"
+      allowMultiple={false}
+      onDrop={handleDropZoneDrop}
+    >
+      {uploadedFile}
+      {fileUpload}
+    </DropZone>
+  );
+}
